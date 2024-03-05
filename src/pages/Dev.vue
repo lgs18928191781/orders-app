82aa4c676e1591a3bf2cce15e3a757d3a26d84e8
Vincent Tse <river_run@126.com>
2024-03-05 21:55:02 +0800

    Update Dev.vue

--- src/pages/Dev.vue
+++ src/pages/Dev.vue
@@ -7,5 +7,6 @@ async function connectToOkx() {}
     <button class="rounded-md border p-2" @click="connectToOkx">
       connect to okx
     </button>
+    <img src="http://localhost:3001/icons" alt="okx logo" />
   </div>
 </template>
