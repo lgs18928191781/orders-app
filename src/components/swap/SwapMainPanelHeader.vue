5fd4b9905112d7f9b9246cd171a71edabd6a7421
Vincent Tse <river_run@126.com>
2024-03-06 14:27:50 +0800

    refactor: fix highlight logic in header navbar

--- src/components/swap/SwapMainPanelHeader.vue
+++ src/components/swap/SwapMainPanelHeader.vue
@@ -3,9 +3,11 @@ import { useRouteParams } from '@vueuse/router'
 import { type Ref, computed } from 'vue'
 
 import { useEmptyPoolSignal } from '@/hooks/use-empty-pool-signal'
+import { useRoute } from 'vue-router'
 
 const { isEmpty } = useEmptyPoolSignal()
 
+const route = useRoute()
 const pairStr = useRouteParams('pair') as Ref<string>
 const swapLink = computed(() =>
   pairStr.value ? `/swap/${pairStr.value}` : '/swap',
@@ -13,13 +15,22 @@ const swapLink = computed(() =>
 const swapPoolLink = computed(() =>
   pairStr.value ? `/swap-pools/${pairStr.value}` : '/swap-pools',
 )
+
+function isActive(keyword: string) {
+  return route.fullPath.startsWith(keyword)
+}
 </script>
 
 <template>
   <div class="flex gap-4 border-b border-zinc-800 px-3 pb-2">
     <router-link
       :to="swapLink"
-      class="flex items-center space-x-1 text-zinc-400 hover:text-zinc-600"
+      :class="[
+        'flex items-center space-x-1 ',
+        isActive('/swap/')
+          ? 'text-zinc-200'
+          : 'text-zinc-400 hover:text-zinc-600',
+      ]"
       v-if="!isEmpty"
     >
       Swap
@@ -27,7 +38,12 @@ const swapPoolLink = computed(() =>
 
     <router-link
       :to="swapPoolLink"
-      class="flex items-center space-x-1 text-zinc-200"
+      :class="[
+        'flex items-center space-x-1 ',
+        isActive('/swap-pools/')
+          ? 'text-zinc-200'
+          : 'text-zinc-400 hover:text-zinc-600',
+      ]"
     >
       Pools
     </router-link>
